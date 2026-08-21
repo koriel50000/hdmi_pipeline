6274126f0c075e49b82e5332d2ee2681d54fda43
koriel50000 <JZW00040@gmail.com>
2026-08-21 21:55:51 +0900

    update count

--- hls_overlay/_ide/workspace_journal_22812.py
+++ hls_overlay/_ide/workspace_journal_22812.py
@@ -1,4 +1,4 @@
-# 2026-08-21T21:22:54.904392
+# 2026-08-21T19:27:38.698340800
 import vitis
 
 client = vitis.create_client()
@@ -7,7 +7,13 @@ client.set_workspace(path="hls_overlay")
 comp = client.get_component(name="hls_component")
 comp.run(operation="SYNTHESIS")
 
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="SYNTHESIS")
+
 comp.run(operation="PACKAGE")
 
-vitis.dispose()
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="PACKAGE")
 
