66e96e5613816fb05a318c4d5c48a8bcea61905c
koriel50000 <JZW00040@gmail.com>
2026-08-28 16:24:35 +0900

    design_1が変わっていないので戻っていなそう

--- hls_overlay/_ide/workspace_journal.py
+++ hls_overlay/_ide/workspace_journal.py
@@ -1,40 +1,10 @@
-# 2026-08-27T17:32:47.341047100
+# 2026-08-28T16:05:50.894111400
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="hls_overlay")
 
 comp = client.get_component(name="hls_component")
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="PACKAGE")
-
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="PACKAGE")
-
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="PACKAGE")
-
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="PACKAGE")
-
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="PACKAGE")
-
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="PACKAGE")
-
-comp.run(operation="SYNTHESIS")
-
-comp.run(operation="PACKAGE")
-
 comp.run(operation="SYNTHESIS")
 
 comp.run(operation="PACKAGE")
