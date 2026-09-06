4af048a930fc08eb3f034371ab60641a3e43541e
koriel50000 <JZW00040@gmail.com>
2026-09-06 18:00:44 +0900

    スプライトデータの渡し方を考える、まずは縦のみ

--- hls_overlay/src/overlay.cpp
+++ hls_overlay/src/overlay.cpp
@@ -130,8 +130,8 @@ void select_line_sprites(const Detect detects[MAX_DETECTIONS], const ap_uint<8> 
                 const int cx = (detects[i].x1 + detects[i].x2) / 2;
                 line_sprites[count].x1 = cx - size / 2;
                 line_sprites[count].x2 = cx + size / 2;
-                // line_sprites[count].size = size;
-                // line_sprites[count].base = ((y - detects[i].y1) * SPRITE_SIZE / size) * SPRITE_SIZE;
+                line_sprites[count].size = size;
+                line_sprites[count].base = ((y - detects[i].y1) * SPRITE_SIZE / size) * SPRITE_SIZE;
                 line_sprites[count].enable = true;
                 count++;
             }
@@ -153,7 +153,8 @@ void set_sprite_pixel(const LineSprite line_sprites[MAX_LINE_SPRITES], const uin
 #pragma HLS unroll
         if (line_sprites[i].enable && line_sprites[i].x1 <= x && x <= line_sprites[i].x2) {
             // int offset = ((x - line_sprites[i].x1) * SPRITE_SIZE) / line_sprites[i].size;
-            pix.data = 0x0000ff; //sprite_data[line_sprites[i].base + offset];
+            ap_uint<32> rgba = sprite_data[line_sprites[i].base + 200];
+            pix.data = rgba.range(31, 8);
         }
     }
 }
