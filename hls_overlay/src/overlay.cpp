a7c96ece6d88a058e6cf113d5fa78ff33f01fd70
koriel50000 <JZW00040@gmail.com>
2026-09-12 16:08:20 +0900

    color分岐飲み残してbase,offset計算もコメントアウト

--- hls_overlay/src/overlay.cpp
+++ hls_overlay/src/overlay.cpp
@@ -168,10 +168,10 @@ void set_sprite_pixel(LineSprite line_sprites[MAX_LINE_SPRITES], const uint16_t 
 #pragma HLS unroll
         LineSprite& sprite = line_sprites[i];
         if (sprite.enable && sprite.x1 <= x && x <= sprite.x2) {
-            sprite.src_x += sprite.src_dx;
-            const uint16_t base = sprite.src_x >> (16 + 5);
-            const uint8_t offset = (sprite.src_x & 0x1f0000) >> 15;
-            ap_uint<64> data = 0x0; //sprite.linebuf[base];
+            // sprite.src_x += sprite.src_dx;
+            // const uint16_t base = sprite.src_x >> (16 + 5);
+            // const uint8_t offset = (sprite.src_x & 0x1f0000) >> 15;
+            // ap_uint<64> data = sprite.linebuf[base];
             ap_uint<2> color = 0x3; //(data >> offset) & 0x3;
             if (color == 1) {
                 pix.data = 0xff0000;
