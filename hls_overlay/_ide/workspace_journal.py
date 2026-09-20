5b55da42ed6a235ced329fb2df3d19b4bb6f7a4d
koriel50000 <JZW00040@gmail.com>
2026-09-20 20:33:02 +0900

    マージしてやり直し

--- hls_overlay/_ide/workspace_journal.py
+++ hls_overlay/_ide/workspace_journal.py
@@ -13,3 +13,10 @@ comp.run(operation="SYNTHESIS")
 
 comp.run(operation="PACKAGE")
 
+vitis.dispose()
+
+comp = client.get_component(name="hls_component")
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="PACKAGE")
+
