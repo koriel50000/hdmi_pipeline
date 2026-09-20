b2ddf28e8f005fd68493712896f448d3ca0e50c4
koriel50000 <JZW00040@gmail.com>
2026-09-20 21:33:38 +0900

    今度はどうだ

--- hls_overlay/_ide/workspace_journal_10228.py
+++ hls_overlay/_ide/workspace_journal_10228.py
@@ -1,10 +1,14 @@
-# 2026-09-20T21:24:11.082353400
+# 2026-09-13T10:52:12.851623300
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="hls_overlay")
 
 comp = client.get_component(name="hls_component")
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="PACKAGE")
+
 comp.run(operation="SYNTHESIS")
 
 comp.run(operation="PACKAGE")
