81e3b65e287d83534096b990da9e007ba024f167
koriel50000 <JZW00040@gmail.com>
2026-09-20 21:09:58 +0900

    7日前まで戻してみる

--- hls_overlay/_ide/workspace_journal.py
+++ hls_overlay/_ide/workspace_journal.py
@@ -1,10 +1,14 @@
-# 2026-09-20T20:41:36.091030500
+# 2026-09-13T10:52:12.851623300
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="hls_overlay")
 
 comp = client.get_component(name="hls_component")
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="PACKAGE")
+
 comp.run(operation="SYNTHESIS")
 
 comp.run(operation="PACKAGE")
